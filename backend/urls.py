b11b185e2ff93d851ac68ae41c0120971eccbd83
Ibrahim Badwi <ib.a.badwi@gmail.com>
2019-10-03 13:10:17 +0300

    Organize urls file

--- backend/urls.py
+++ backend/urls.py
@@ -7,27 +7,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework import routers
 
-from .api.views import MessageViewSet
-# from .api.views import index_view
-from .api.experiences.views import ExperienceViewSet, TagViewSet
-
-router = routers.DefaultRouter()
-router.register('messages', MessageViewSet)
-router.register('experiences', ExperienceViewSet, base_name="Experience")
-router.register('tags', TagViewSet, base_name="Tag")
-# router.register('comments', CommentViewSet, basename="comments")
 
 urlpatterns = [
+    # main page index
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 
-    # http://localhost:8000/
-    # path('', index_view, name='index'),
-
-    # http://localhost:8000/api/<router-viewsets>
-    path('api/', include(router.urls)),
-
-    # http://localhost:8000/api/admin/
+    # admin dashboard
     path('admin/', admin.site.urls),
 
     # regsiter new user
@@ -37,11 +23,9 @@ urlpatterns = [
     path('api/accounts/', include('rest_auth.urls')),
 
     # update user settings an d profile
-    path('api/accounts/', include('backend.api.accounts.urls')),
+    # path('api/accounts/', include('backend.api.accounts.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api/comments/', include('backend.api.experiences.urls')),
-
-    path('', TemplateView.as_view(template_name='index.html'))
+    # path('api/comments/', include('backend.api.experiences.urls')),
 ]
