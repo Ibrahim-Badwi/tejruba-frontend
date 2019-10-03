9b2ece99982a2a62a1b48dcba80b1cad1889008e
Ibrahim Badwi <ib.a.badwi@gmail.com>
2019-10-03 15:26:28 +0300

    add permissions to views

--- backend/api/users/views.py
+++ backend/api/users/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework import permissions
 
+from ..permissions import IsOwnerOrReadOnly
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -7,8 +9,9 @@ from .serializers import CustomUserSerializer
 class UserList(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-
+    
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly, )
