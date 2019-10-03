75495883b25c38097eeb560662c1f1ea32bba4f2
Ibrahim Badwi <ib.a.badwi@gmail.com>
2019-10-03 14:34:32 +0300

    add id field to customuserserializer fields

--- backend/api/users/serializers.py
+++ backend/api/users/serializers.py
@@ -6,5 +6,5 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'date_joined', 'bio', 
+        fields = ('id', 'username', 'email', 'date_joined', 'bio', 
                   'birth_date','country','job', 'settings')
