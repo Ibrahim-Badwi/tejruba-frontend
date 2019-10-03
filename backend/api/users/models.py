44b2f896577924eac2c6822bcd480ad9c1d10ea9
Ibrahim Badwi <ib.a.badwi@gmail.com>
2019-10-03 14:04:14 +0300

    move field from customuser model to settings model

--- backend/api/users/models.py
+++ backend/api/users/models.py
@@ -5,13 +5,13 @@ from django.dispatch import receiver
 
 
 class Settings(models.Model):
+    user = models.OneToOneField('CustomUser', on_delete=models.CASCADE, 
+                                related_name='settings', null=True, blank=True)
     notify_when_reply = models.BooleanField(default=True)
     notify_when_comment = models.BooleanField(default=True)
 
 
 class CustomUser(AbstractUser):
-    settings = models.OneToOneField(Settings, on_delete=models.CASCADE, related_name='user', null=True, blank=True)
-
     bio = models.TextField(max_length=200, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     country = models.CharField(max_length=30, null=True, blank=True)
@@ -23,4 +23,4 @@ class CustomUser(AbstractUser):
 def create_update_settings(sender, instance, created, **kwargs):
     if created:
         Settings.objects.create(user=instance)
-    instance.settings.save()
+    # instance.settings.save()
